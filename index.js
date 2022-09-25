141bf80bf27b02e5000cb87ccd131086761b3c36
Pramodh V <pramodhvenkatraman@gmail.com>
2022-09-25 11:16:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
